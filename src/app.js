248d6c219d8a3ab717184f8a424fd51d44662a07
hatcec <37028859+hatcec@users.noreply.github.com>
2021-10-31 22:43:33 +0300

    first

---
+++ src/app.js
@@ -0,0 +1,23 @@
+console.log("merhaba kodlama.io");
+//var dolarDun=9.20
+//dolarDun="ankara"   
+let dolarDun=9.30
+{
+    let dolarDun=9.20
+}
+console.log(dolarDun)
+const euroDun=11.2
+//euroDun=11 const readly tiptir
+console.log(euroDun)
+
+//array
+let konutKredileri=["konut kredisi","emlak konut kredisi","kamu konut kredisi","özel konut kredileri"]
+console.log("<ul>")
+for(let i=0;i<konutKredileri.length;i++) 
+{
+ console.log("<li>"+konutKredileri[i]+"</li>")      
+}
+console.log("</ul>")
+
+
+ 
